ce700aa6f84acd8e4a48f139602c0952e913aa8d
Denis Rouzaud <denis.rouzaud@gmail.com>
2016-08-04 10:43:09 +0200

    Python3 and PyQt5  compatible

--- configurereloaderbase.py
+++ configurereloaderbase.py
@@ -2,12 +2,11 @@
 
 # Form implementation generated from reading ui file 'configurereloaderbase.ui'
 #
-# Created: Sat Aug 21 00:27:45 2010
-#      by: PyQt4 UI code generator 4.7.3
+# Created by: PyQt5 UI code generator 5.6
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_ConfigureReloaderDialogBase(object):
     def setupUi(self, ConfigureReloaderDialogBase):
@@ -19,42 +18,43 @@ class Ui_ConfigureReloaderDialogBase(object):
         icon.addPixmap(QtGui.QPixmap(":/plugins/plugin_reloader/reload-conf.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         ConfigureReloaderDialogBase.setWindowIcon(icon)
         ConfigureReloaderDialogBase.setWhatsThis("")
-        self.gridLayout_2 = QtGui.QGridLayout(ConfigureReloaderDialogBase)
+        self.gridLayout_2 = QtWidgets.QGridLayout(ConfigureReloaderDialogBase)
         self.gridLayout_2.setObjectName("gridLayout_2")
-        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.buttonBox = QtGui.QDialogButtonBox(ConfigureReloaderDialogBase)
+        self.buttonBox = QtWidgets.QDialogButtonBox(ConfigureReloaderDialogBase)
         self.buttonBox.setFocusPolicy(QtCore.Qt.NoFocus)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setCenterButtons(True)
         self.buttonBox.setObjectName("buttonBox")
         self.horizontalLayout.addWidget(self.buttonBox)
         self.gridLayout_2.addLayout(self.horizontalLayout, 6, 0, 1, 4)
-        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout_2.addItem(spacerItem, 3, 3, 1, 1)
-        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout_2.addItem(spacerItem1, 3, 0, 1, 1)
-        self.comboPlugin = QtGui.QComboBox(ConfigureReloaderDialogBase)
+        self.comboPlugin = QtWidgets.QComboBox(ConfigureReloaderDialogBase)
         self.comboPlugin.setMinimumSize(QtCore.QSize(220, 0))
         self.comboPlugin.setFocusPolicy(QtCore.Qt.WheelFocus)
         self.comboPlugin.setObjectName("comboPlugin")
         self.gridLayout_2.addWidget(self.comboPlugin, 3, 1, 1, 1)
-        spacerItem2 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        spacerItem2 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.gridLayout_2.addItem(spacerItem2, 4, 1, 1, 1)
-        spacerItem3 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        spacerItem3 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.gridLayout_2.addItem(spacerItem3, 0, 1, 1, 1)
-        self.label = QtGui.QLabel(ConfigureReloaderDialogBase)
+        self.label = QtWidgets.QLabel(ConfigureReloaderDialogBase)
         self.label.setObjectName("label")
         self.gridLayout_2.addWidget(self.label, 1, 1, 1, 1)
         self.label.setBuddy(self.comboPlugin)
 
         self.retranslateUi(ConfigureReloaderDialogBase)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), ConfigureReloaderDialogBase.accept)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), ConfigureReloaderDialogBase.reject)
+        self.buttonBox.accepted.connect(ConfigureReloaderDialogBase.accept)
+        self.buttonBox.rejected.connect(ConfigureReloaderDialogBase.reject)
         QtCore.QMetaObject.connectSlotsByName(ConfigureReloaderDialogBase)
 
     def retranslateUi(self, ConfigureReloaderDialogBase):
-        ConfigureReloaderDialogBase.setWindowTitle(QtGui.QApplication.translate("ConfigureReloaderDialogBase", "Configure Plugin reloader", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("ConfigureReloaderDialogBase", " Select the plugin you want to reload", None, QtGui.QApplication.UnicodeUTF8))
+        _translate = QtCore.QCoreApplication.translate
+        ConfigureReloaderDialogBase.setWindowTitle(_translate("ConfigureReloaderDialogBase", "Configure Plugin reloader"))
+        self.label.setText(_translate("ConfigureReloaderDialogBase", " Select the plugin you want to reload"))
 
-import resources_rc
+from . import resources_rc
