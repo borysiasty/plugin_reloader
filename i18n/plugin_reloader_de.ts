2aefc4d6f5222ea78c8ea7f328e562ac77ec6532
Werner Macho <werner.macho@iqsoft.com>
2021-09-21 12:51:22 +0200

    added german translation files

---
+++ i18n/plugin_reloader_de.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>ConfigureReloaderDialogBase</name>
+    <message>
+        <location filename="../configurereloaderbase.ui" line="30"/>
+        <source>Configure Plugin reloader</source>
+        <translation>Plugin-Reloader-Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../configurereloaderbase.ui" line="89"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Push a notification about the successful reload to the QGIS message bar.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Chceck this&lt;/span&gt;, it you like to be sure you reloaded the rigth plugin.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Uncheck this&lt;/span&gt;, if you hate trivial notifications covering error messages fom the plugin.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Sende eine Benachrichtigung über das erfolgreiche Neuladen an die QGIS-Nachrichtenleiste.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Aktiviere&lt;/span&gt; diese Option,um zu überprüfen ob das richtige Plugin neu geladen wurde.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Deaktiviere&lt;/span&gt; diese Option, wenn triviale Benachrichtigungen zu Fehlermeldungen des Plugins nicht angezeigt werden sollen.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../configurereloaderbase.ui" line="92"/>
+        <source>Display a notification once the plugin is reloaded</source>
+        <translation>Benachrichtigung anzeigen, sobald das Plugin neu geladen wurde</translation>
+    </message>
+    <message>
+        <location filename="../configurereloaderbase.ui" line="39"/>
+        <source>Select &amp;the plugin you want to reload</source>
+        <translation>Selek&amp;tion des neu zu ladenden Plugins</translation>
+    </message>
+    <message>
+        <location filename="../configurereloaderbase.ui" line="68"/>
+        <source>Commands to run before reloading</source>
+        <translation>Befehle, die vor dem Neuladen ausgeführt werden sollen</translation>
+    </message>
+    <message>
+        <location filename="../configurereloaderbase.ui" line="75"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;QGIS will try to execute any commands typed here in a shell before reloading the plugin.&lt;/p&gt;
+&lt;p&gt;This can be useful, for example, if you need to copy the new source code into the QGIS plugins directory.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;QGIS wird versuchen, alle hier eingegebenen Befehle in einer Shell auszuführen, bevor das Plugin neu geladen wird.&lt;/p&gt;
+&lt;p&gt;Dies kann nützlich sein, wenn der neue Quellcode in das QGIS-Plugin-Verzeichnis kopiert werden muss.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>ReloaderPlugin</name>
+    <message>
+        <location filename="../reloader_plugin.py" line="148"/>
+        <source>Reload plugin: </source>
+        <translation>Plugin neu laden: </translation>
+    </message>
+    <message>
+        <location filename="../reloader_plugin.py" line="180"/>
+        <source>Reload chosen plugin</source>
+        <translation>Ausgewähltes Plugin neu laden</translation>
+    </message>
+    <message>
+        <location filename="../reloader_plugin.py" line="249"/>
+        <source>Reload plugin: {}</source>
+        <translation>Plugin neu laden: {}</translation>
+    </message>
+    <message>
+        <location filename="../reloader_plugin.py" line="204"/>
+        <source>&amp;Plugin Reloader</source>
+        <translation>&amp;Plugin-Reloader</translation>
+    </message>
+    <message>
+        <location filename="../reloader_plugin.py" line="191"/>
+        <source>Configure</source>
+        <translation>Konfigurieren</translation>
+    </message>
+    <message>
+        <location filename="../reloader_plugin.py" line="197"/>
+        <source>Choose a plugin to be reloaded</source>
+        <translation>Wählen Sie ein Plugin aus, das neu geladen werden soll</translation>
+    </message>
+    <message>
+        <location filename="../reloader_plugin.py" line="240"/>
+        <source>&lt;b&gt;{}&lt;/b&gt; reloaded.</source>
+        <translation>&lt;b&gt;{}&lt;/b&gt; neu geladen.</translation>
+    </message>
+</context>
+</TS>
