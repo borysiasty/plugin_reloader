ac1c6e4c499c02bc91402638689c213a90e29a82
Borys Jurgiel <info@borysjurgiel.pl>
2021-11-13 12:05:13 +0100

    Update translations

--- i18n/plugin_reloader_de.ts
+++ i18n/plugin_reloader_de.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="de_DE">
+<!DOCTYPE TS><TS version="2.0" language="de_DE" sourcelanguage="">
 <context>
     <name>ConfigureReloaderDialogBase</name>
     <message>
@@ -11,67 +10,92 @@
     <message>
         <location filename="../configurereloaderbase.ui" line="89"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Push a notification about the successful reload to the QGIS message bar.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Chceck this&lt;/span&gt;, it you like to be sure you reloaded the rigth plugin.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Uncheck this&lt;/span&gt;, if you hate trivial notifications covering error messages fom the plugin.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Sende eine Benachrichtigung über das erfolgreiche Neuladen an die QGIS-Nachrichtenleiste.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Aktiviere&lt;/span&gt; diese Option,um zu überprüfen ob das richtige Plugin neu geladen wurde.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Deaktiviere&lt;/span&gt; diese Option, wenn triviale Benachrichtigungen zu Fehlermeldungen des Plugins nicht angezeigt werden sollen.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation type="obsolete">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Sende eine Benachrichtigung über das erfolgreiche Neuladen an die QGIS-Nachrichtenleiste.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Aktiviere&lt;/span&gt; diese Option,um zu überprüfen ob das richtige Plugin neu geladen wurde.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Deaktiviere&lt;/span&gt; diese Option, wenn triviale Benachrichtigungen zu Fehlermeldungen des Plugins nicht angezeigt werden sollen.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../configurereloaderbase.ui" line="92"/>
+        <location filename="../configurereloaderbase.ui" line="48"/>
         <source>Display a notification once the plugin is reloaded</source>
         <translation>Benachrichtigung anzeigen, sobald das Plugin neu geladen wurde</translation>
     </message>
     <message>
-        <location filename="../configurereloaderbase.ui" line="39"/>
+        <location filename="../configurereloaderbase.ui" line="115"/>
         <source>Select &amp;the plugin you want to reload</source>
         <translation>Selek&amp;tion des neu zu ladenden Plugins</translation>
     </message>
     <message>
         <location filename="../configurereloaderbase.ui" line="68"/>
         <source>Commands to run before reloading</source>
-        <translation>Befehle, die vor dem Neuladen ausgeführt werden sollen</translation>
+        <translation type="obsolete">Befehle, die vor dem Neuladen ausgeführt werden sollen</translation>
     </message>
     <message>
-        <location filename="../configurereloaderbase.ui" line="75"/>
+        <location filename="../configurereloaderbase.ui" line="131"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;QGIS will try to execute any commands typed here in a shell before reloading the plugin.&lt;/p&gt;
 &lt;p&gt;This can be useful, for example, if you need to copy the new source code into the QGIS plugins directory.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;QGIS wird versuchen, alle hier eingegebenen Befehle in einer Shell auszuführen, bevor das Plugin neu geladen wird.&lt;/p&gt;
 &lt;p&gt;Dies kann nützlich sein, wenn der neue Quellcode in das QGIS-Plugin-Verzeichnis kopiert werden muss.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
+    <message>
+        <location filename="../configurereloaderbase.ui" line="45"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Push a notification about the successful reload to the QGIS message bar.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Check this&lt;/span&gt;, if you like to be sure you reloaded the right plugin.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Uncheck this&lt;/span&gt;, if you hate trivial notifications covering error messages from the plugin.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../configurereloaderbase.ui" line="90"/>
+        <source>Note: any %PluginName% will be replaced by the plugin&apos;s name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../configurereloaderbase.ui" line="135"/>
+        <source>Run the commands below before reloading</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>ReloaderPlugin</name>
     <message>
-        <location filename="../reloader_plugin.py" line="148"/>
+        <location filename="../reloader_plugin.py" line="166"/>
         <source>Reload plugin: </source>
         <translation>Plugin neu laden: </translation>
     </message>
     <message>
-        <location filename="../reloader_plugin.py" line="180"/>
+        <location filename="../reloader_plugin.py" line="198"/>
         <source>Reload chosen plugin</source>
         <translation>Ausgewähltes Plugin neu laden</translation>
     </message>
     <message>
-        <location filename="../reloader_plugin.py" line="249"/>
+        <location filename="../reloader_plugin.py" line="281"/>
         <source>Reload plugin: {}</source>
         <translation>Plugin neu laden: {}</translation>
     </message>
     <message>
-        <location filename="../reloader_plugin.py" line="204"/>
+        <location filename="../reloader_plugin.py" line="222"/>
         <source>&amp;Plugin Reloader</source>
         <translation>&amp;Plugin-Reloader</translation>
     </message>
     <message>
-        <location filename="../reloader_plugin.py" line="191"/>
+        <location filename="../reloader_plugin.py" line="209"/>
         <source>Configure</source>
         <translation>Konfigurieren</translation>
     </message>
     <message>
-        <location filename="../reloader_plugin.py" line="197"/>
+        <location filename="../reloader_plugin.py" line="215"/>
         <source>Choose a plugin to be reloaded</source>
         <translation>Wählen Sie ein Plugin aus, das neu geladen werden soll</translation>
     </message>
     <message>
         <location filename="../reloader_plugin.py" line="240"/>
         <source>&lt;b&gt;{}&lt;/b&gt; reloaded.</source>
-        <translation>&lt;b&gt;{}&lt;/b&gt; neu geladen.</translation>
+        <translation type="obsolete">&lt;b&gt;{}&lt;/b&gt; neu geladen.</translation>
+    </message>
+    <message>
+        <location filename="../reloader_plugin.py" line="244"/>
+        <source>Plugin &lt;b&gt;{}&lt;/b&gt; not found.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../reloader_plugin.py" line="271"/>
+        <source>&lt;b&gt;{}&lt;/b&gt; reloaded in {} ms.</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
--- i18n/plugin_reloader_pl.ts
+++ i18n/plugin_reloader_pl.ts
@@ -9,69 +9,79 @@
         <translation>Konfiguracja Plugin Reloadera</translation>
     </message>
     <message>
-        <location filename="../configurereloaderbase.ui" line="89"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Push a notification about the successful reload to the QGIS message bar.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Chceck this&lt;/span&gt;, it you like to be sure you reloaded the rigth plugin.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Uncheck this&lt;/span&gt;, if you hate trivial notifications covering error messages fom the plugin.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Wyświetlaj potwierdzenie przeładowania wtyczki w pasku powiadomień QGIS-a.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Zaznacz&lt;/span&gt;, jeśli chcesz mieć pewność, że przeładowujesz właściwą wtyczkę.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Odznacz&lt;/span&gt;, jeśli wolisz mniej powiadomień.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
-    </message>
-    <message>
-        <location filename="../configurereloaderbase.ui" line="92"/>
+        <location filename="../configurereloaderbase.ui" line="48"/>
         <source>Display a notification once the plugin is reloaded</source>
         <translation>Wyświetlaj powiadomienie o przeładowaniu wtyczki</translation>
     </message>
     <message>
-        <location filename="../configurereloaderbase.ui" line="39"/>
+        <location filename="../configurereloaderbase.ui" line="115"/>
         <source>Select &amp;the plugin you want to reload</source>
         <translation>Wybierz &amp;wtyczkę do przeładowania</translation>
     </message>
     <message>
-        <location filename="../configurereloaderbase.ui" line="68"/>
-        <source>Commands to run before reloading</source>
-        <translation>Dodatkowe polecenia powłoki do wykonania</translation>
-    </message>
-    <message>
-        <location filename="../configurereloaderbase.ui" line="75"/>
+        <location filename="../configurereloaderbase.ui" line="131"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;QGIS will try to execute any commands typed here in a shell before reloading the plugin.&lt;/p&gt;
 &lt;p&gt;This can be useful, for example, if you need to copy the new source code into the QGIS plugins directory.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Wprowadź komendy wiersza poleceń, które mają być wykonane przed przeładowaniem wtyczki.&lt;/p&gt;
 &lt;p&gt;Może to być na przykład kopiowanie kodu źródłowego wtyczki z katalogu roboczego do katalogu wtyczek QGIS.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
+    <message>
+        <location filename="../configurereloaderbase.ui" line="45"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Push a notification about the successful reload to the QGIS message bar.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Check this&lt;/span&gt;, if you like to be sure you reloaded the right plugin.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Uncheck this&lt;/span&gt;, if you hate trivial notifications covering error messages from the plugin.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Wyświetlaj potwierdzenie przeładowania wtyczki w pasku powiadomień QGIS-a.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Zaznacz&lt;/span&gt;, jeśli chcesz mieć pewność, że przeładowujesz właściwą wtyczkę.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Odznacz&lt;/span&gt;, jeśli wolisz mniej powiadomień.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../configurereloaderbase.ui" line="90"/>
+        <source>Note: any %PluginName% will be replaced by the plugin&apos;s name.</source>
+        <translation>Możesz uzyć zmiennej %PluginName% zamiast nazwy wtyczki.</translation>
+    </message>
+    <message>
+        <location filename="../configurereloaderbase.ui" line="135"/>
+        <source>Run the commands below before reloading</source>
+        <translation>Uruchom poniższe polecenia przed przeładowaniem</translation>
+    </message>
 </context>
 <context>
     <name>ReloaderPlugin</name>
     <message>
-        <location filename="../reloader_plugin.py" line="148"/>
+        <location filename="../reloader_plugin.py" line="166"/>
         <source>Reload plugin: </source>
         <translation>Przeładu wtyczkę: </translation>
     </message>
     <message>
-        <location filename="../reloader_plugin.py" line="180"/>
+        <location filename="../reloader_plugin.py" line="198"/>
         <source>Reload chosen plugin</source>
         <translation>Przeładuj wybraną wtyczkę</translation>
     </message>
     <message>
-        <location filename="../reloader_plugin.py" line="249"/>
+        <location filename="../reloader_plugin.py" line="281"/>
         <source>Reload plugin: {}</source>
         <translation>Przeładuj wtyczkę: {}</translation>
     </message>
     <message>
-        <location filename="../reloader_plugin.py" line="204"/>
+        <location filename="../reloader_plugin.py" line="222"/>
         <source>&amp;Plugin Reloader</source>
         <translation>&amp;Plugin Reloader</translation>
     </message>
     <message>
-        <location filename="../reloader_plugin.py" line="191"/>
+        <location filename="../reloader_plugin.py" line="209"/>
         <source>Configure</source>
         <translation>Ustawienia</translation>
     </message>
     <message>
-        <location filename="../reloader_plugin.py" line="197"/>
+        <location filename="../reloader_plugin.py" line="215"/>
         <source>Choose a plugin to be reloaded</source>
         <translation>Wybierz wtyczkę do przeładowania</translation>
     </message>
     <message>
-        <location filename="../reloader_plugin.py" line="240"/>
-        <source>&lt;b&gt;{}&lt;/b&gt; reloaded.</source>
-        <translation>Wtyczka &lt;b&gt;{}&lt;/b&gt; przeładowana.</translation>
+        <location filename="../reloader_plugin.py" line="244"/>
+        <source>Plugin &lt;b&gt;{}&lt;/b&gt; not found.</source>
+        <translation>Nie znaleziono wtyczki &lt;b&gt;{}&lt;/b&gt;.</translation>
+    </message>
+    <message>
+        <location filename="../reloader_plugin.py" line="271"/>
+        <source>&lt;b&gt;{}&lt;/b&gt; reloaded in {} ms.</source>
+        <translation>Wtyczka &lt;b&gt;{}&lt;/b&gt; przeładowana w {} ms.</translation>
     </message>
 </context>
 </TS>
